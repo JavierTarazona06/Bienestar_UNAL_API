a7678b7aa10b94ae109efef58f56931390d505e7
Valeria Mora <vmoras@unal.edu.co>
2023-06-04 16:42:50 -0500

    Add more endpoints

--- main.py
+++ main.py
@@ -1,32 +1,38 @@
-from fastapi import FastAPI
-import time
 import os
-from sqlalchemy import create_engine
+import time
+import datetime
+from typing import Any
+from fastapi import FastAPI
+from dotenv import load_dotenv
 from sqlalchemy.engine import URL
 from mysql.connector import errors
+from sqlalchemy import create_engine
+from contextlib import asynccontextmanager
 from fastapi.encoders import jsonable_encoder
-from typing import Any
 
-app = FastAPI()
+load_dotenv()
 
 url = URL.create(
     drivername=os.environ["DRIVER"],
-    username=os.environ["USERNAME"],
+    username=os.environ["NAME-USER"],
     password=os.environ["PASSWORD"],
-    host=os.environ["HOST"],
+    host=os.environ["HOST-NAME"],
     database=os.environ["DATABASE"],
     port=int(os.environ["PORT"])
 )
 
-print(type(os.environ["DRIVER"]), os.environ["DRIVER"])
-print(type(os.environ["USERNAME"]), os.environ["USERNAME"])
-print(type(os.environ["PASSWORD"]), os.environ["PASSWORD"])
-print(type(os.environ["HOST"]), os.environ["HOST"])
-print(type(os.environ["DATABASE"]), os.environ["DATABASE"])
-print(type(os.environ["PORT"]), os.environ["PORT"])
-
-
 engine = create_engine(url)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    pass
+    yield
+    # Close the connection with the database
+    connection.close()
+
+app = FastAPI(lifespan=lifespan)
+
 
 while True:
     try:
@@ -39,45 +45,68 @@ while True:
 
 @app.get("/")
 async def root():
+    return jsonable_encoder('MAIN PAGE')
+
+
+@app.get("/citas_medicas_disponibles")
+async def select_citamedicas():
     rows = call_procedure('pas_citas_disponibles')
     return jsonable_encoder(rows)
 
 
-@app.get("/hello/{user_id}")
-async def say_hello(user_id: str):
+@app.get("/cita_medica_agendada/{user_id}")
+async def select_citamedicas_user(user_id: str):
     rows = call_procedure('pas_citas_agendadas', user_id)
     return jsonable_encoder(rows)
 
 
-def call_procedure(procedure: str, *args: Any) -> list:
-    if args:
-        print(f"{procedure} {args[0]}")
-    else:
-        print(f"{procedure}")
+@app.put("/cancelar_cita_medica/{user_id}")
+async def delete_citamedica_user(user_id: int, fecha: datetime.datetime, especialidad: str):
+    rows = call_procedure('pas_delete_cita_medica', user_id, fecha, especialidad)
+    return jsonable_encoder(rows)
 
+
+@app.put("/agendar_cita_medica/{user_id}")
+async def add_citamedica_user(user_id: int, fecha: datetime.datetime, especialidad: str):
+    rows = call_procedure('pas_add_cita_medica', user_id, fecha, especialidad)
+    return jsonable_encoder(rows)
+
+
+@app.get("/resultado_cita_medica/{user_id}")
+async def check_citamedica_user(user_id: int):
+    rows = call_procedure('pas_check_resultados', user_id)
+    return jsonable_encoder(rows)
+
+
+def call_procedure(procedure: str, *args: Any) -> list:
+    """
+    Returns a list with all the rows given a procedure and arguments
+
+    :param procedure: function to call
+    :param args: arguments for the procedure
+    """
     cursor = connection.cursor()
     try:
         cursor.callproc(procedure, list(args))
     except errors.ProgrammingError as e:
-        print(e)
-        return []
+        return [e]
     except errors.DatabaseError as e:
-        print(e)
-        return []
+        return [e]
 
     answer = None
     for result in cursor.stored_results():
         answer = result
 
+    print(answer)
+
     rows = []
     if answer is not None:
         for row in answer:
             rows.append(row)
+    else:
+        rows = ['Done']
 
     cursor.close()
     connection.commit()
-    # connection.close()
 
     return rows
-
-
